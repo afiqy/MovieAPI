d1a5c25c2cc120a17220dd96cd3d86a208f67a89
Mohd Afiq Faruqy <35352377+afiqy@users.noreply.github.com>
2024-09-21 16:52:03 +0800

    add MovieList Response

---
+++ MovieAPI/DTOs/MovieListResponse.cs
@@ -0,0 +1,12 @@
+﻿using MovieAPI.Models;
+
+namespace MovieAPI.DTOs
+{
+    public class MovieListResponse
+    {
+        public int Page { get; set; }
+        public List<Movie> Results { get; set; }
+        public int Total_Pages { get; set; }
+        public int Total_Results { get; set; }
+    }
+}
