56cb8e2ad9a1a917d99cb5ed0782fb2f71b14e9a
Mohd Afiq Faruqy <35352377+afiqy@users.noreply.github.com>
2024-09-21 17:12:37 +0800

    update movie table-add details

---
+++ MovieAPI/Migrations/20240921091152_UpdateMovieTableAddFewColumnDetails.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMovieTableAddFewColumnDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Adult",
+                table: "Movies",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Overview",
+                table: "Movies",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Poster_Path",
+                table: "Movies",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Release_Date",
+                table: "Movies",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Vote_Average",
+                table: "Movies",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Vote_Count",
+                table: "Movies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Adult",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Overview",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Poster_Path",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Release_Date",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Vote_Average",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Vote_Count",
+                table: "Movies");
+        }
+    }
+}
--- MovieAPI/Models/Movie.cs
+++ MovieAPI/Models/Movie.cs
@@ -6,6 +6,13 @@
         public string Title { get; set; }
         public string Description { get; set; }
         public string ExternalId { get; set; }
+        public string Overview { get; internal set; }
+        public string Poster_Path { get; internal set; }
+        public string Release_Date { get; internal set; }
+        public bool Adult { get; internal set; }
+        public double Vote_Average { get; internal set; }
+        public int Vote_Count { get; internal set; }
+
         public ICollection<Favourite> Favourites { get; set; }
     }
 }
